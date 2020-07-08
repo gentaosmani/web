ba8a3485a1eae93eb164e2e34c510de0b58f0824
gentaosmani <63254762+gentaosmani@users.noreply.github.com>
2020-07-09 00:33:34 +0200

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,61 @@
+<footer class="footer-distributed">
+ 
+        <div class="footer-left">
+ 
+        <h3>e-Health</h3>
+ 
+        <p class="footer-links">
+        <a href="#">Home</a>
+    ·
+        <a href="#">Blog</a>
+    ·
+        <a href="#">Pricing</a>
+    ·
+        <a href="#">About</a>
+    ·
+        <a href="#">Faq</a>
+    ·
+        <a href="#">Contact</a>
+        </p>
+ 
+        <p class="footer-company-name">e-Health &copy; 2010</p>
+        </div>
+ 
+        <div class="footer-center">
+ 
+        <div>
+        <i class="fa fa-map-marker"></i>
+        <p><span>21 Revolution Street</span> Delhi, India</p>
+        </div>
+ 
+        <div>
+        <i class="fa fa-phone"></i>
+        <p>+1 555 123456</p>
+        </div>
+ 
+        <div>
+        <i class="fa fa-envelope"></i>
+        <p><a href="mailto:support@company.com">contact@webdevtrick.com</a></p>
+        </div>
+ 
+        </div>
+ 
+        <div class="footer-right">
+ 
+        <p class="footer-company-about">
+        <span>About the company</span>
+    The E-health website provides health services online.
+        </p>
+ 
+        <div class="footer-icons">
+ 
+        <a href="#"><i class="fa fa-facebook"></i></a>
+        <a href="#"><i class="fa fa-twitter"></i></a>
+        <a href="#"><i class="fa fa-linkedin"></i></a>
+        <a href="#"><i class="fa fa-github"></i></a>
+ 
+        </div>
+ 
+        </div>
+ 
+        </footer>
