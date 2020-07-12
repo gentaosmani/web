1ab50ed158c714cbf1e912094f26ee1e47af7b6b
gentaosmani <63254762+gentaosmani@users.noreply.github.com>
2020-07-12 05:07:17 +0200

    login changed

--- loginForm.php
+++ loginForm.php
@@ -1,14 +1,3 @@
-<!--<?php/*
- require './controller/AuthController.php';
-
-  $user = new AuthController;
-
-  if(isset($_POST['submit'])){
-      $user->login($_POST);
-  }
-
-  */?>-->
-
 <?php
     require './controller/AuthController.php';
 
@@ -16,8 +5,8 @@
 
     if(isset($_POST['submit'])) {
         $user->login($_POST);
-     header('Location: ./myMedicalData.php');
-    }
+     header('Location: ./AfterLogin.php');
+    } 
 ?>
 
 <!DOCTYPE html>
@@ -34,6 +23,148 @@
   <title style="margin-left: 100px"> 
   E-Health
 </title>
+<style type="text/css">
+  
+      .inputt-icons i { 
+            position: absolute; 
+        } 
+          
+        .inputt-icons { 
+            width: 100%; 
+            margin-top: 30px; 
+
+
+        } 
+          
+        .icon { 
+            padding: 10px; 
+            color: green; 
+            min-width: 50px; 
+            text-align: center;
+            
+        } 
+          
+        .inputt-field { 
+            width: 380px; 
+            padding: 10px; 
+            text-align: center; 
+            border:1px solid white;
+            border-radius: 35px;
+          
+
+
+        } 
+
+
+    .containerr {
+      display: block;
+      position: relative;
+      padding-left: 35px;
+      margin-bottom: 12px;
+      cursor: pointer;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+      color: white;
+       width: 100%
+    }
+
+    
+    .containerr input {
+      position: absolute;
+      opacity: 0;
+      cursor: pointer;
+      height: 0;
+      width: 0;
+    }
+
+    
+    .checkmark {
+      position: absolute;
+      top: 0;
+      left: 0;
+      height: 25px;
+      width: 25px;
+      background-color: white;
+      border-radius: 50%;
+    }
+
+    
+    .containerr:hover input ~ .checkmark {
+      background-color: #ccc;
+    }
+
+    
+    .containerr input:checked ~ .checkmark {
+      background-color: green;
+    }
+
+    
+    .checkmark:after {
+      content: "";
+      position: absolute;
+      display: none;
+    }
+
+    .containerr input:checked ~ .checkmark:after {
+      display: block;
+    }
+
+    
+    .containerr .checkmark:after {
+      top: 9px;
+      left: 9px;
+      width: 8px;
+      height: 8px;
+      border-radius: 50%;
+      background: white;
+    }
+    
+    .slide:hover, .slide:focus {
+      box-shadow: inset 150px 0 0 0 white;
+    }
+
+    .slide {
+      color:white;
+      transition: 0.3s;
+    }
+    .slide:hover, .slide:focus {
+      border-color: #24daac;
+      color: #24daac;
+    }
+
+    .slide {
+      background: #24daac;
+      border: 5px solid #24daac;
+      padding: 0.4em 1.8em;
+      font-weight: bolder;
+      border-radius: 20px;
+      font-size: 15px;
+      text-decoration:none;
+      width: 120px;
+      margin-left: 130px;
+    }
+
+
+    .slide2:hover, .slide2:focus {
+      box-shadow: inset 140px 0 0 0 white;
+    }
+
+    .slide2 {
+      color:white;
+      transition: 0.3s;
+    }
+    .slide2:hover, .slide2:focus {
+      border-color: #24daac;
+      color: #24daac;
+    }
+    .responsive {
+  max-width: 100%;
+  height: auto;
+  
+}
+</style>
 
 </head>
 <body>
@@ -63,9 +194,11 @@ function myFunction() {
 }
 </script>
 </header>
-<div class="container flex-direction">
+
+
+<div class="container flex-direction" id="hide-div">
   <div class="div1">
-  <img class="image" src="gallery/userrr.png" width="130px" height="130px" style="">
+  <img class="image" src="css/gallery/userrr.png" width="130px" height="130px" style="">
   </div>
   <div class="div2">
      <script type="text/javascript">
@@ -94,7 +227,7 @@ function myFunction() {
         </script>
        
 
-   <form action="" method="POST" name="myForm" > 
+   <form action="" method="POST" onsubmit="return(validate())" name="myForm" > 
     <!--<form action="loginValidation.php" onsubmit="return(validate())" method="POST" name="myForm" > -->
             
             <div class="inputt-icons"> 
@@ -124,16 +257,31 @@ function myFunction() {
         </label>
     </div>-->
     <br>
-    <input  class="slide" type="submit" value="submit" name="submit">
+    <input  class="slide" type="submit" value="Log in" name="submit">
   </form>
   <br>
   
  </div>
+
 </div>
+ <?php if(isset($_SESSION['name'])): ?>
+    <script>
+      document.getElementById("hide-div").style.display="none";
+    </script>
+    <br><br><br><br>
+    <div style="display: block; justify-content: center;">
+    <p style="text-align: center; font-weight: bold; font-size: 30px; color: white"> YOU ARE ALREADY LOGGED IN!</p>
+  <a href="AfterLogin.php" class="slide" style="width: 190px; margin-left: 585px" >Back to main page</a> <br> <br> <br>
+   <a href="logout.php" class="slide" style="width: 190px; margin-left: 618px" >Log out</a>
+  </div>
+  <?php endif; ?>
+<br>
 </div>
 
 <!--FOOTER-->
 <?php include 'includes/footer.php'; ?>
+
+
  
 </body>
 </html>
